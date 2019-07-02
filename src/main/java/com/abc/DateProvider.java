768c93454dc30c1cdf0e8dcc2a1de3a12d8d5b57
thisdj3 <50526770+thisdj3@users.noreply.github.com>
2019-07-02 09:39:51 +0100

    Add files via upload

--- src/main/java/com/abc/DateProvider.java
+++ src/main/java/com/abc/DateProvider.java
@@ -4,15 +4,23 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateProvider {
-    private static DateProvider instance = null;
+	private static DateProvider instance = null;
 
-    public static DateProvider getInstance() {
-        if (instance == null)
-            instance = new DateProvider();
-        return instance;
-    }
+	public static DateProvider getInstance() {
+		if (instance == null)
+			instance = new DateProvider();
+		return instance;
+	}
 
-    public Date now() {
-        return Calendar.getInstance().getTime();
-    }
+	public static int moveDateForward = 0;
+
+	public Date now() {
+
+		Date dt = new Date();
+		Calendar c = Calendar.getInstance();
+		c.setTime(dt);
+		c.add(Calendar.DATE, moveDateForward);
+		dt = c.getTime();
+		return dt;
+	}
 }
